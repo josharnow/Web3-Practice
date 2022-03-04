436992ffe8dbdaab7562cf8046bc620e24cd5c14
"Josh Arnow" <"1476967+josharnow@users.noreply.github.com ">
2022-03-04 17:07:47 -0500

    Completed Solidity NFT Course

---
+++ .learneth/Solidity NFT Course/4. Token Creation/erc721TokenCreation.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts@4.4.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.4.0/access/Ownable.sol";
+
+contract Geometry is ERC721, Ownable {
+    constructor() ERC721("Geometry", "GEO") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/QmVrsYxXh5PzTfkKZr1MfUN6PotJj8VQkGQ3kGyBNVKtqp/";
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
+
+// contract MyToken is ERC721, Ownable {
+//     constructor() ERC721("MyToken", "MTK") {}
+
+//     function _baseURI() internal pure override returns (string memory) {
+//         return "https://ipfs.io/ipfs/QmUYLUKwqX6CaZxeiYGwmAYeEkeTsV4cHNZJmCesuu3xKy/";
+//     }
+
+//     function safeMint(address to, uint256 tokenId) public onlyOwner {
+//         _safeMint(to, tokenId);
+//     }
+// }
