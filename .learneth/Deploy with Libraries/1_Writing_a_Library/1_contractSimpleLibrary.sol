9be5e3b656f01bb61d1ef38cbac7d6227695c75c
Joshua D. Arnow <1476967+josharnow@users.noreply.github.com>
2022-03-05 20:17:29 -0500

    Completed "Deploy with Libraries"

---
+++ .learneth/Deploy with Libraries/1_Writing_a_Library/1_contractSimpleLibrary.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract test {
+    function get () public returns (uint) {
+        // should call a library method which returns `3`
+        return LibraryForTest.getFromLib();  
+    }
+}
+
+// Put the library here
+library LibraryForTest {
+    function getFromLib() public returns (uint) {
+        return 3;
+    }
+}
